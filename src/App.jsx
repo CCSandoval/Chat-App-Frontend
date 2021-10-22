c5d98f89b553fc1eb9d8892dedcf4dbbae7647d4
Cristian <krcamilo0417@gmail.com>
2021-10-22 12:58:05 -0500

    Fix CORS

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import chat from "./img/chat.png";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 
-const socket = io.connect("https://powerful-sands-35615.herokuapp.com/");
+const socket = io.connect("https://powerful-sands-35615.herokuapp.com");
 
 function App() {
   const [username, setUsername] = useState("");
